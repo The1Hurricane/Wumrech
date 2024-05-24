b05e8a392006823aaabcec9444e4dda9d962900b
The1Hurricane <julian.delgado@gmx.de>
2024-05-24 21:33:02 +0200

    commit

--- Währungsumrechner/src/wumrech_main/Wumrech_main.java
+++ Währungsumrechner/src/wumrech_main/Wumrech_main.java
@@ -2,7 +2,10 @@ package wumrech_main;
 
 public class Wumrech_main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		char dollarSymbol = '$', euroSymbol = '€';
+	    System.out.println(dollarSymbol);
+	    System.out.println(euroSymbol);
+		System.out.println("check");
 		
 	}
 
